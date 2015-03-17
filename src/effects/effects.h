09dbbaa0e6a2c6bf455ce67017eb2cf4c07f63c7
Daan de Lange <idaany@gmail.com>
2015-03-17 13:25:08 +0100

    project cleanup

--- src/effects/effects.h
+++ src/effects/effects.h
@@ -11,13 +11,13 @@
 
 // add all effect files to this file and they'll all be automatically included in files that need all effects
 #include "basicEffect.h"
-#include "imageEffect.h"
+//#include "imageEffect.h"
 //#include "shaderEffect.h"
-#include "videoEffect.h"
+//#include "videoEffect.h"
 //#include "gifEffect.h"
 #include "lineEffect.h"
-#include "imageBeatEffect.h"
-#include "meshRenderer3D.h"
-#include "videoStreamEffect.h"
+//#include "imageBeatEffect.h"
+//#include "meshRenderer3D.h"
+//#include "videoStreamEffect.h"
 
 #endif
